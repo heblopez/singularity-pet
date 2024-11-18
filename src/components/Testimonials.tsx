4b0ffbd917d82ad821c1449ba6eeec0b8d604fd1
Heberth López <heberth.lopez.19@gmail.com>
2024-11-18 18:46:55 -0500

    fix: :ambulance: Updated styles to make responsive NotFound page

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -14,7 +14,7 @@ const testimonials: ICarouselCard[] = repeatObject(
 
 export default function Testimonials() {
   return (
-    <div className='w-full h-[650px] bg-transparent mt-[124px] relative flex flex-col items-center gap-20'>
+    <div className='w-[1593px] h-[650px] bg-transparent mt-[124px] relative flex flex-col items-center gap-20'>
       <h2 className='text-tomato text-5xl leading-[64px] font-bold max-w-[750px]'>
         Here's what pet owners have to say about Fetch! Pet Care...
       </h2>
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -25,35 +25,35 @@ function renderPawPrints(numberOfPaws: number) {
 export default function NotFoundPage() {
   const navigate = useNavigate()
   return (
-    <div className='absolute overflow-hidden top-0 left-0 w-full bg-[url("/bg-404.svg")] bg-cover'>
-      <div className='flex items-center justify-center h-screen w-full min-w-80 max-w-7xl 3xl:max-w-screen-2xl mx-auto px-8'>
-        <div className='w-1/2 hidden md:flex md:justify-center'>
+    <div className='absolute overflow-hidden top-0 left-0 w-full min-h-screen bg-[url("/bg-404.svg")] bg-cover bg-center'>
+      <div className='flex items-center justify-center min-h-screen w-full max-w-7xl 3xl:max-w-screen-2xl mx-auto p-8'>
+        <div className='w-1/2 hidden xl:flex xl:justify-center'>
           <div className='relative w-[400px] h-[400px] flex justify-center items-end'>
             <div className='absolute w-[80%] h-[80%] bg-banana-yellow rounded-[50%] overflow-hidden -z-[0]'></div>
             <img src={dogPng} alt='chihuahua' className='w-full z-10' />
           </div>
         </div>
 
-        <div className='w-[70%] flex flex-col gap-9 text-smokey-gray'>
-          <h1 className='text-[84px] leading-[100px] font-museo font-bold max-w-[654px] text-tomato z-50 text-shadow-white'>
+        <div className='w-full xl:w-[70%] flex flex-col gap-9 text-smokey-gray'>
+          <h1 className='text-5xl leading-normal xl:text-[84px] xl:leading-[100px] font-museo font-bold text-center z-50 bg-gradient-to-br from-banana-yellow to-tomato bg-clip-text text-transparent'>
             Error 404: Page Not Found
           </h1>
-          <p className='text-xl leading-8 max-w-[595px] z-50'>
+          <div className='text-xl leading-8 z-50 bg-white-smoke shadow-2xl xl:shadow-none xl:bg-transparent p-4 sm:p-8 sm:mx-8 xl:mx-16 rounded-3xl xl:bg-none xl:p-0 xl:rounded-none'>
             We're sorry, but the page you're looking for doesn't exist.
             <br />
             It seems that you got a little lost browsing our web site, however below are
             some buttons to help you get back on track.
-          </p>
-          <div className='flex self-start w-full max-w-max gap-12 items-center z-50'>
+          </div>
+          <div className='flex flex-col mt-6 gap-6 sm:flex-row sm:justify-center self-start w-full xl:gap-12 items-center z-50'>
             <SingularityButton
               type='submit'
-              className='font-museo font-bold text-white-smoke hover:bg-tomato'
+              className='font-museo font-bold text-white-smoke hover:bg-tomato flex items-center'
             >
               <Link to='/'>Back to Home</Link>
             </SingularityButton>
             <SingularityButton
               type='submit'
-              className='font-museo font-bold text-white-smoke hover:bg-tomato'
+              className='font-museo font-bold text-white-smoke hover:bg-tomato flex items-center'
               onClick={() => navigate(-1)}
             >
               Back to Previous Page
